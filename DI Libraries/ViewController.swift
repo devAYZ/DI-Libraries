2e700030821d7a89bc52d955ce70f591c6f23df8
devAYZ <fatokimiemman@gmail.com>
2024-01-28 16:39:02 +0100

    use Swinject container

--- DI Libraries/ViewController.swift
+++ DI Libraries/ViewController.swift
@@ -8,25 +8,54 @@
 import UIKit
 import Swinject
 
+protocol ColorProviding {
+    var color: UIColor { get }
+}
+
+class ColorProvider: ColorProviding {
+    var color: UIColor {
+        let color: [UIColor] = [.systemRed, .systemTeal, .systemBlue]
+        var randomColor = color.randomElement()!
+        return randomColor
+    }
+}
+
 class ViewController: UIViewController {
     
     private lazy var button: UIButton = {
         let button = UIButton(frame: .init(x: .zero, y: .zero, width: 180, height: 48))
+        button.backgroundColor = .red.withAlphaComponent(0.12)
         button.setTitle("Proceed", for: .normal)
-        button.center = view.center
-        button.addTarget(self, action: #selector(handleProceed), for: .touchUpInside)
         button.setTitleColor(.red, for: .normal)
         button.titleLabel?.font = UIFont.systemFont(ofSize: 24, weight: .regular)
         button.layer.borderWidth = 0.35
         button.layer.cornerRadius = 10
-        button.backgroundColor = .red.withAlphaComponent(0.12)
+        button.addTarget(self, action: #selector(handleProceed), for: .touchUpInside)
+        button.center = view.center
         view.addSubview(button)
         return button
     }()
     
     @objc private func handleProceed() {
         
+        guard let vc = container.resolve(ColorViewController.self) else { return }
+        present(vc, animated: true)
     }
+    
+    let container: Container = {
+        let container = Container()
+        
+        
+        container.register(ColorViewController.self) { resolver in
+            ColorViewController(colorProvider: resolver.resolve(ColorProviding.self))
+        }
+        
+        container.register(ColorProviding.self) { _ in
+            ColorProvider()
+        }
+        return container
+        
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,8 +67,20 @@ class ViewController: UIViewController {
 
 class ColorViewController: UIViewController {
     
+    var colorProvider: ColorProviding?
+    
+    init(colorProvider: ColorProviding? = nil) {
+        self.colorProvider = colorProvider
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = colorProvider?.color
     }
     
 }
