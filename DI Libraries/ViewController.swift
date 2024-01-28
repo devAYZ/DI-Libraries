9fdafaa99a2b2143452d9fd5668a2ba1f10384cd
devAYZ <fatokimiemman@gmail.com>
2024-01-28 15:36:53 +0100

    add button

--- DI Libraries/ViewController.swift
+++ DI Libraries/ViewController.swift
@@ -9,12 +9,38 @@ import UIKit
 import Swinject
 
 class ViewController: UIViewController {
+    
+    private lazy var button: UIButton = {
+        let button = UIButton(frame: .init(x: .zero, y: .zero, width: 180, height: 48))
+        button.setTitle("Proceed", for: .normal)
+        button.center = view.center
+        button.addTarget(self, action: #selector(handleProceed), for: .touchUpInside)
+        button.setTitleColor(.red, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 24, weight: .regular)
+        button.layer.borderWidth = 0.35
+        button.layer.cornerRadius = 10
+        button.backgroundColor = .red.withAlphaComponent(0.12)
+        view.addSubview(button)
+        return button
+    }()
+    
+    @objc private func handleProceed() {
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(button)
     }
 
 
+}
+
+class ColorViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
 }
 
